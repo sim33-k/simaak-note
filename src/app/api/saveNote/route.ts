93642dba681d65babf2f66b2b275228dcfcf77e6
SW33TSTUFF <simaakniyaz@gmail.com>
2026-01-04 15:49:24 +0530

    fix(route): change variable declaration from let to const for noteId and editorState

--- src/app/api/saveNote/route.ts
+++ src/app/api/saveNote/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    let { noteId, editorState } = body;
+    const { noteId, editorState } = body;
     if (!editorState || !noteId) {
       return new NextResponse("Missing editorState or noteId", { status: 400 });
     }
